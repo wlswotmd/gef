c6eb4c7b47a974304416fa42519eb37a2401d3a0
bata24 <tooooooooooooooooooooohsaka@gmail.com>
2023-06-02 12:45:31 +0900

    fix trans

--- gef.py
+++ gef.py
@@ -39031,13 +39031,15 @@ class U2dCommand(GenericCommand):
 
 
 @register_command
-class PackCommand(GenericCommand):
+class TransCommand(GenericCommand):
     """Translate various (pack, pack-hex, unpack, tohex, unhex, byteswap)."""
     _cmdline_ = "trans"
     _category_ = "09-a. Misc - Translation"
 
     parser = argparse.ArgumentParser(prog=_cmdline_)
     parser.add_argument('value', metavar='VALUE', help='the value or string you want to translate.')
+    parser.add_argument('-n', '--no-pager', action='store_true', help='do not use less.')
+    parser.add_argument('-v', '--verbose', action='store_true', help='enable verbose mode.')
     _syntax_ = parser.format_help()
 
     _example_ = "{:s} 0xdeadbeef\n".format(_cmdline_)
@@ -39047,46 +39049,48 @@ class PackCommand(GenericCommand):
     def do_invoke(self, args):
         self.dont_repeat()
 
+        self.out = []
+
         try:
             value = int(args.value, 0)
-            gef_print(titlify("pack"))
-            gef_print("pack8:         {}".format(p8(value & 0xff)))
-            gef_print("pack16:        {}".format(p16(value & 0xffff)))
-            gef_print("pack32:        {}".format(p32(value & 0xffffffff)))
-            gef_print("pack64:        {}".format(p64(value & 0xffffffffffffffff)))
+            self.out.append(titlify("pack"))
+            self.out.append("pack8:         {}".format(p8(value & 0xff)))
+            self.out.append("pack16:        {}".format(p16(value & 0xffff)))
+            self.out.append("pack32:        {}".format(p32(value & 0xffffffff)))
+            self.out.append("pack64:        {}".format(p64(value & 0xffffffffffffffff)))
             low = value & 0xffffffffffffffff
             high = (value >> 64) & 0xffffffffffffffff
             val128 = p64(low) + p64(high)
-            gef_print("pack128:       {}".format(val128))
+            self.out.append("pack128:       {}".format(val128))
 
-            gef_print(titlify("pack-hex"))
-            gef_print("pack8-hex:     {}".format(p8(value & 0xff).hex()))
-            gef_print("pack16-hex:    {}".format(p16(value & 0xffff).hex()))
-            gef_print("pack32-hex:    {}".format(p32(value & 0xffffffff).hex()))
-            gef_print("pack64-hex:    {}".format(p64(value & 0xffffffffffffffff).hex()))
-            gef_print("pack128-hex:   {}".format(val128.hex()))
+            self.out.append(titlify("pack-hex"))
+            self.out.append("pack8-hex:     {}".format(p8(value & 0xff).hex()))
+            self.out.append("pack16-hex:    {}".format(p16(value & 0xffff).hex()))
+            self.out.append("pack32-hex:    {}".format(p32(value & 0xffffffff).hex()))
+            self.out.append("pack64-hex:    {}".format(p64(value & 0xffffffffffffffff).hex()))
+            self.out.append("pack128-hex:   {}".format(val128.hex()))
         except ValueError:
             pass
 
         try:
             value = codecs.escape_decode(args.value)[0] + b"\0" * 16
-            gef_print(titlify("unpack"))
-            gef_print("unpack8:       {:#04x}".format(u8(value[:1])))
-            gef_print("unpack16:      {:#06x}".format(u16(value[:2])))
-            gef_print("unpack32:      {:#010x}".format(u32(value[:4])))
-            gef_print("unpack64:      {:#018x}".format(u64(value[:8])))
+            self.out.append(titlify("unpack"))
+            self.out.append("unpack8:       {:#04x}".format(u8(value[:1])))
+            self.out.append("unpack16:      {:#06x}".format(u16(value[:2])))
+            self.out.append("unpack32:      {:#010x}".format(u32(value[:4])))
+            self.out.append("unpack64:      {:#018x}".format(u64(value[:8])))
             low, high = value[:8], value[8:16]
-            gef_print("unpack128:     {:#034x}".format((u64(high) << 64) | u64(low)))
+            self.out.append("unpack128:     {:#034x}".format((u64(high) << 64) | u64(low)))
         except binascii.Error:
             pass
 
         try:
             value = codecs.escape_decode(args.value)[0]
-            gef_print(titlify("tohex"))
+            self.out.append(titlify("tohex"))
             hexed = binascii.hexlify(value)
-            gef_print("tohex:         {}".format(hexed))
+            self.out.append("tohex:         {}".format(hexed))
             hexed_null = b"00".join(slicer(hexed, 2)) + b"00"
-            gef_print("tohex w/NULL:  {}".format(hexed_null))
+            self.out.append("tohex w/NULL:  {}".format(hexed_null))
         except binascii.Error:
             pass
 
@@ -39095,10 +39099,10 @@ class PackCommand(GenericCommand):
                 value = binascii.unhexlify(args.value[2:])
             else:
                 value = binascii.unhexlify(args.value)
-            gef_print(titlify("unhex"))
-            gef_print("unhex:         {}".format(value))
+            self.out.append(titlify("unhex"))
+            self.out.append("unhex:         {}".format(value))
             value_null = b"\x00".join(slicer(value, 1)) + b"\x00"
-            gef_print("unhex w/NULL:  {}".format(value_null))
+            self.out.append("unhex w/NULL:  {}".format(value_null))
         except binascii.Error:
             pass
 
@@ -39110,19 +39114,19 @@ class PackCommand(GenericCommand):
             uQbe = lambda a: struct.unpack(">Q", a)[0]
             converted32 = ube(p(value))
             converted64 = uQbe(pQ(value))
-            gef_print(titlify("byteswap"))
-            gef_print("byteswap-64:   {:#018x}".format(converted64))
-            gef_print("byteswap-32:   {:#010x}".format(converted32))
+            self.out.append(titlify("byteswap"))
+            self.out.append("byteswap-64:   {:#018x}".format(converted64))
+            self.out.append("byteswap-32:   {:#010x}".format(converted32))
         except ValueError:
             pass
 
         try:
             value = int(args.value, 0)
-            gef_print(titlify("integer"))
-            gef_print("hex:           {:#x}".format(value))
-            gef_print("dec:           {:d}".format(value))
-            gef_print("oct:           {:#o}".format(value))
-            gef_print("bin:           {:#b}".format(value))
+            self.out.append(titlify("integer"))
+            self.out.append("hex:           {:#x}".format(value))
+            self.out.append("dec:           {:d}".format(value))
+            self.out.append("oct:           {:#o}".format(value))
+            self.out.append("bin:           {:#b}".format(value))
             out = ""
             x = value
             while x:
@@ -39134,31 +39138,92 @@ class PackCommand(GenericCommand):
                     out = "_" + out
                 x >>= 1
             splitted_value = "0b" + out[1:]
-            gef_print("bin w/sep:     {:s}".format(splitted_value))
+            self.out.append("bin w/sep:     {:s}".format(splitted_value))
         except ValueError:
             pass
 
         try:
             value = int(args.value, 0)
-            gef_print(titlify("signed"))
+            self.out.append(titlify("signed"))
             pQ = lambda a: struct.pack("<Q", a & 0xffffffffffffffff)
             uq = lambda a: struct.unpack("<q", a)[0]
-            gef_print("u2i-64:        {:#018x}".format(uq(pQ(value))))
+            self.out.append("u2i-64:        {:#018x}".format(uq(pQ(value))))
             p = lambda a: struct.pack("<I", a & 0xffffffff)
             ui = lambda a: struct.unpack("<i", a)[0]
-            gef_print("u2i-32:        {:#010x}".format(ui(p(value))))
+            self.out.append("u2i-32:        {:#010x}".format(ui(p(value))))
         except ValueError:
             pass
 
         try:
             value = codecs.escape_decode(args.value)[0]
-            gef_print(titlify("string"))
-            gef_print("str:           {}".format(value))
+            self.out.append(titlify("string"))
+            self.out.append("str:           {}".format(value))
             value_null = b"\x00".join(slicer(value, 1)) + b"\x00"
-            gef_print("str w/NULL:    {}".format(value_null))
+            self.out.append("str w/NULL:    {}".format(value_null))
         except ValueError:
             pass
 
+        if not args.verbose:
+            gef_print('\n'.join(self.out), less=not args.no_pager)
+            return
+
+        # verbose mode
+        try:
+            if args.value.startswith("0x"):
+                value = binascii.unhexlify(args.value[2:])
+            else:
+                value = binascii.unhexlify(args.value)
+            self.out.append(titlify("unhex - XOR/ADD"))
+            l = len(value) * 4 + 3
+            for i in range(0x100):
+                xored = b"".join(bytes([x ^ i]) for x in value)
+                added = b"".join(bytes([(x + i) & 0xff]) for x in value)
+                self.out.append("xor/add-{:02X}:    {:{:d}s} {:{:d}s}".format(i, str(xored), l, str(added), l))
+
+            self.out.append(titlify("unhex - caesar"))
+            for i in range(26):
+                slided = []
+                for x in value:
+                    if ord('A') <= x <= ord('Z'):
+                        x += i
+                        if x > ord('Z'):
+                            x -= ord('Z')
+                    elif ord('a') <= x <= ord('z'):
+                        x += i
+                        if x > ord('z'):
+                            x -= ord('z')
+                    slided.append(x)
+                self.out.append("caesar-{:02d}:    {}".format(i, bytes(slided)))
+        except binascii.Error:
+            pass
+
+        try:
+            value = codecs.escape_decode(args.value)[0]
+            self.out.append(titlify("str - XOR/ADD"))
+            l = len(value) * 4 + 3
+            for i in range(0x100):
+                xored = b"".join(bytes([x ^ i]) for x in value)
+                added = b"".join(bytes([(x + i) & 0xff]) for x in value)
+                self.out.append("xor/add-{:02X}:    {:{:d}s} {:{:d}s}".format(i, str(xored), l, str(added), l))
+
+            self.out.append(titlify("str - caesar"))
+            for i in range(26):
+                slided = []
+                for x in value:
+                    if ord('A') <= x <= ord('Z'):
+                        x += i
+                        if x > ord('Z'):
+                            x -= ord('Z')
+                    elif ord('a') <= x <= ord('z'):
+                        x += i
+                        if x > ord('z'):
+                            x -= ord('z')
+                    slided.append(x)
+                self.out.append("caesar-{:02d}:    {}".format(i, bytes(slided)))
+        except ValueError:
+            pass
+
+        gef_print('\n'.join(self.out), less=not args.no_pager)
         return
 
 
