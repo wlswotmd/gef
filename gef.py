3fb23b9cbac9984aff79e846e7f455d29a37e04c
bata24 <tooooooooooooooooooooohsaka@gmail.com>
2023-06-18 11:47:42 +0900

    fix get_terminal_size

--- gef.py
+++ gef.py
@@ -100,6 +100,7 @@ import collections
 import configparser
 import ctypes
 import datetime
+import fcntl
 import functools
 import hashlib
 import inspect
@@ -117,6 +118,7 @@ import struct
 import subprocess
 import sys
 import tempfile
+import termios
 import time
 import traceback
 import urllib.request
@@ -9269,26 +9271,11 @@ def load_libc_args():
 
 def get_terminal_size():
     """Return the current terminal size."""
-    if platform.system() == "Windows":
-        hStdErr = -12
-        herr = ctypes.windll.kernel32.GetStdHandle(hStdErr)
-        csbi = ctypes.create_string_buffer(22)
-        res = ctypes.windll.kernel32.GetConsoleScreenBufferInfo(herr, csbi)
-        if res:
-            _, _, _, _, _, left, top, right, bottom, _, _ = struct.unpack("hhhhHhhhhhh", csbi.raw)
-            tty_columns = right - left + 1
-            tty_rows = bottom - top + 1
-            return tty_rows, tty_columns
-        else:
-            return 600, 100
-    else:
-        import fcntl
-        import termios
-        try:
-            tty_rows, tty_columns = struct.unpack("hh", fcntl.ioctl(1, termios.TIOCGWINSZ, "1234"))
-            return tty_rows, tty_columns
-        except OSError:
-            return 600, 100
+    try:
+        tty_rows, tty_columns = struct.unpack("hh", fcntl.ioctl(1, termios.TIOCGWINSZ, "1234"))
+        return tty_rows, tty_columns
+    except OSError:
+        return 600, 100
 
 
 def get_generic_arch(module, prefix, arch, mode, big_endian, to_string=False, extra=()):
